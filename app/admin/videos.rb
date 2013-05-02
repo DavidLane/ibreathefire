09d3f0f9345bb957b4e09b7b9d3909af94997ad0
David Lane <david.lane@logistikgroup.com>
2013-05-02 20:08:10 +0100

    Removing events

--- app/admin/videos.rb
+++ app/admin/videos.rb
@@ -1,10 +1,3 @@
-ActiveAdmin.register Video, :as => "Featured Video" do
-  scope :featured, :default => true
-  controller {
-  	with_role :admin
-  }  
-end
-
 ActiveAdmin.register Video do
   controller {
   	with_role :admin
